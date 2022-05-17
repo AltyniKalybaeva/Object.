ba6f5ab7780d7c78efe5c074692d08117feb580e
rosekook <altynaikalybaikyzy@gmail.com>
2022-05-17 19:27:53 +0600

    texst

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// let altynai = {
+//     name:"Altynai",
+//     age:'20',
+//     hobbies:{
+//         first:' watch doram',
+//         second:" to cook sweets",
+//     }                                       
+// }
+
+//  console.log( Object.keys(altynai))
+//        console.log( Object.values(altynai))
+// function moon(){
+//     alert("My name is Goldenmoon")
+// }
+// moon()
+
+// function alt(){
+//     console.log(20+20)
+// }
+// alt()
+// function baer(a,b,c,d,e,f){
+//     console.log(a+b*c/d+e-f)
+// }
+// baer(20,10,30,5,12,45)
+// let bar=[10,20,5,3]
+//     function mon( rose){
+//         console.log(10+20-5+3)
+//     }
+// mon()
+// let bar=[10,20,5,3]
+
+//     function mon( rose){    
+// document.write(rose)
+//     }
+// mon(bar)
+
+    let button = document.getElementById('pluse')
+    button.addEventListener ('click', mow)
+    let p =document.getElementById('p')
+    function mow(){
+        p.style.display('none')
+    }
+    
+  
